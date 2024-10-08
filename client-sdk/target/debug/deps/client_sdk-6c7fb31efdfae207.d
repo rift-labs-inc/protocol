9b8601d431b968dcd306c7504d5113b616a83d1c
Cliff Syner <c4syner@gmail.com>
2024-10-08 14:17:55 -0700

    fix: remove dead code

--- client-sdk/target/debug/deps/client_sdk-6c7fb31efdfae207.d
+++
@@ -1,5 +0,0 @@
-/Users/alpinevm/Development/riftco/protocol/client-sdk/target/debug/deps/libclient_sdk-6c7fb31efdfae207.rmeta: src/lib.rs
-
-/Users/alpinevm/Development/riftco/protocol/client-sdk/target/debug/deps/client_sdk-6c7fb31efdfae207.d: src/lib.rs
-
-src/lib.rs:
